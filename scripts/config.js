3ada7540c6f95130cdc14009ef9b45d2a7560fba
sahil69-create <sgamer4448@gmail.com>
2025-12-19 13:52:50 +0530

    Add application configuration to config.js

---
+++ scripts/config.js
@@ -0,0 +1,13 @@
+window.APP_CONFIG = {
+  brand: "Campus Ka Jaika",
+  phoneE164: "917206123642",
+  displayPhone: "+91 72061 23642",
+  email: "hello@campuskajaika.com",
+  hours: "10:00 AM – 10:00 PM",
+  mapEmbedUrl: "https://www.google.com/maps/embed?pb=",
+  instagramUrl: "https://www.instagram.com/campuskajaika/",
+  offers: [
+    { title: "Combo Saver", detail: "Buy 2, get 10% off", tag: "Today" },
+    { title: "Student Special", detail: "Free soft drink with any meal", tag: "Limited" }
+  ]
+}
