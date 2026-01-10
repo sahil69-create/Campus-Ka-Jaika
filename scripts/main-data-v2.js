fe247c2be2a6ab1c3d982ea8d3097676ec859991
sahil69-create <sgamer4448@gmail.com>
2026-01-10 10:02:23 +0530

    Create main-data-v2.js

---
+++ scripts/main-data-v2.js
@@ -0,0 +1,303 @@
+window.MENU_DATA = {
+  "categories": [
+    {
+      "id": "sandwich",
+      "name": "Sandwich",
+      "items": [
+        { "id": "snd-veg", "name": "Veg. Sandwich", "price": 79, "desc": "" },
+        { "id": "snd-tandoori-paneer", "name": "Tandoori Paneer Sandwich", "price": 99, "desc": "" },
+        { "id": "snd-cheese-corn", "name": "Cheese Corn sandwich", "price": 89, "desc": "" },
+        { "id": "snd-cold", "name": "Cold Sandwich", "price": 79, "desc": "" },
+        { "id": "snd-cheese-grilled", "name": "Cheese Grilled Sandwich", "price": 119, "desc": "" },
+        { "id": "snd-campus", "name": "Campus Special Sandwich", "price": 125, "desc": "" }
+      ]
+    },
+    {
+      "id": "pasta",
+      "name": "Pasta",
+      "items": [
+        { "id": "pst-white", "name": "White Sauce Pasta", "price": 99, "desc": "" },
+        { "id": "pst-red", "name": "Red Sauce Pasta", "price": 99, "desc": "" },
+        { "id": "pst-mix", "name": "Mix Sauce Pasta", "price": 109, "desc": "" },
+        { "id": "pst-tandoori", "name": "Tandoori Pasta", "price": 109, "desc": "" },
+        { "id": "pst-makhani", "name": "Makhani Pasta", "price": 109, "desc": "" },
+        { "id": "pst-campus", "name": "Campus Special Pasta", "price": 125, "desc": "" }
+      ]
+    },
+    {
+      "id": "momos",
+      "name": "Momos & Sides",
+      "items": [
+        { "id": "momo-veg", "name": "Veg Momos (Steam, Fries)", "price": 79, "desc": "" },
+        { "id": "momo-tandoori", "name": "Tandoori Veg Momos", "price": 109, "desc": "" },
+        { "id": "spring-roll", "name": "Spring Roll", "price": 89, "desc": "" },
+        { "id": "noodles", "name": "Noodles", "price": 79, "desc": "" },
+        { "id": "corn-chaat", "name": "Corn Chaat", "price": 89, "desc": "" }
+      ]
+    },
+    {
+      "id": "garlic-bread",
+      "name": "Garlic Bread",
+      "items": [
+        { "id": "gb-cheese", "name": "Cheese Garlic Bread", "price": 89, "desc": "" },
+        { "id": "gb-stuff", "name": "Stuff Garlic Bread", "price": 110, "desc": "" },
+        { "id": "gb-paneer-stuff", "name": "PaneerStuff Garlic Bread", "price": 149, "desc": "" },
+        { "id": "gb-campus", "name": "Campus Speacial Garlic Bread", "price": 155, "desc": "" }
+      ]
+    },
+    {
+      "id": "burger",
+      "name": "Burger",
+      "items": [
+        { "id": "bgr-aloo", "name": "Aloo Burger", "price": 59, "desc": "" },
+        { "id": "bgr-veg", "name": "Veg. Burger", "price": 69, "desc": "" },
+        { "id": "bgr-cheese", "name": "Cheese Burger", "price": 89, "desc": "" },
+        { "id": "bgr-paneer", "name": "Paneer Loaded Burger", "price": 89, "desc": "" },
+        { "id": "bgr-pizza", "name": "Pizza Burger", "price": 99, "desc": "" },
+        { "id": "bgr-campus", "name": "Campus Special Burger", "price": 109, "desc": "" }
+      ]
+    },
+    {
+      "id": "fries",
+      "name": "Fries",
+      "items": [
+        { "id": "fry-classic", "name": "Classic Fries", "price": 79, "desc": "" },
+        { "id": "fry-peri", "name": "Peri Peri Fries", "price": 89, "desc": "" },
+        { "id": "fry-cheese", "name": "Cheese Fries", "price": 89, "desc": "" },
+        { "id": "fry-tandoori", "name": "Tandoori Fries", "price": 99, "desc": "" },
+        { "id": "fry-campus", "name": "Campus Special Fries", "price": 109, "desc": "" }
+      ]
+    },
+    {
+      "id": "maggie",
+      "name": "Maggie",
+      "items": [
+        { "id": "mag-plain", "name": "Plain Maggie", "price": 49, "desc": "" },
+        { "id": "mag-masala", "name": "Masala Maggie", "price": 59, "desc": "" },
+        { "id": "mag-corn", "name": "Sweet Corn Maggie", "price": 69, "desc": "" },
+        { "id": "mag-cheese", "name": "Cheese Maggie", "price": 79, "desc": "" },
+        { "id": "mag-cheese-corn", "name": "Cheese Corn Maggie", "price": 89, "desc": "" },
+        { "id": "mag-tandoori", "name": "Tandoori Maggie", "price": 89, "desc": "" },
+        { "id": "mag-makhni", "name": "Makhni Maggie", "price": 99, "desc": "" },
+        { "id": "mag-campus", "name": "Campus Special Maggie", "price": 119, "desc": "" }
+      ]
+    },
+    {
+      "id": "wrap",
+      "name": "Wrap",
+      "items": [
+        { "id": "wrp-veg", "name": "Veg. Wrap", "price": 99, "desc": "" },
+        { "id": "wrp-chilli-paneer", "name": "Chilli Paneer Wrap", "price": 120, "desc": "" },
+        { "id": "wrp-tandoori-paneer", "name": "Tandoori Paneer Wrap", "price": 120, "desc": "" },
+        { "id": "wrp-campus", "name": "Campus Special Wrap", "price": 135, "desc": "" }
+      ]
+    },
+    {
+      "id": "veggies-special",
+      "name": "Veggies Special",
+      "items": [
+        { "id": "piz-dcm-s", "name": "Double Cheese Margherita (S)", "price": 150, "desc": "Loaded with Delicious Cheese" },
+        { "id": "piz-dcm-m", "name": "Double Cheese Margherita (M)", "price": 300, "desc": "Loaded with Delicious Cheese" },
+        { "id": "piz-dcm-l", "name": "Double Cheese Margherita (L)", "price": 400, "desc": "Loaded with Delicious Cheese" },
+
+        { "id": "piz-veg-lov-s", "name": "Veggie Lover (S)", "price": 150, "desc": "Onion, Capsicum, Tomato, Mushroom" },
+        { "id": "piz-veg-lov-m", "name": "Veggie Lover (M)", "price": 300, "desc": "Onion, Capsicum, Tomato, Mushroom" },
+        { "id": "piz-veg-lov-l", "name": "Veggie Lover (L)", "price": 400, "desc": "Onion, Capsicum, Tomato, Mushroom" },
+
+        { "id": "piz-spice-s", "name": "Spice Lover (S)", "price": 150, "desc": "Julienne Capsicum, Jalapeno, Crispy Onion, Red Peprika, Spice Herb, Delicious Cheese" },
+        { "id": "piz-spice-m", "name": "Spice Lover (M)", "price": 300, "desc": "Julienne Capsicum, Jalapeno, Crispy Onion, Red Peprika, Spice Herb, Delicious Cheese" },
+        { "id": "piz-spice-l", "name": "Spice Lover (L)", "price": 400, "desc": "Julienne Capsicum, Jalapeno, Crispy Onion, Red Peprika, Spice Herb, Delicious Cheese" },
+
+        { "id": "piz-veg-del-s", "name": "Veg Delight (S)", "price": 150, "desc": "Onion, Capsicum, Jalapeno, Corn, Mushroom" },
+        { "id": "piz-veg-del-m", "name": "Veg Delight (M)", "price": 300, "desc": "Onion, Capsicum, Jalapeno, Corn, Mushroom" },
+        { "id": "piz-veg-del-l", "name": "Veg Delight (L)", "price": 400, "desc": "Onion, Capsicum, Jalapeno, Corn, Mushroom" },
+
+        { "id": "piz-pan-spl-s", "name": "Paneer Special (S)", "price": 150, "desc": "Fresh Paneer, Crispy Onion, Julienne Capsicum, Tomato, Cheese" },
+        { "id": "piz-pan-spl-m", "name": "Paneer Special (M)", "price": 300, "desc": "Fresh Paneer, Crispy Onion, Julienne Capsicum, Tomato, Cheese" },
+        { "id": "piz-pan-spl-l", "name": "Paneer Special (L)", "price": 400, "desc": "Fresh Paneer, Crispy Onion, Julienne Capsicum, Tomato, Cheese" },
+
+        { "id": "piz-veg-blast-s", "name": "Veg. Blast (S)", "price": 150, "desc": "Onion, Capsicum, Corn, Mushroom, Tomato" },
+        { "id": "piz-veg-blast-m", "name": "Veg. Blast (M)", "price": 300, "desc": "Onion, Capsicum, Corn, Mushroom, Tomato" },
+        { "id": "piz-veg-blast-l", "name": "Veg. Blast (L)", "price": 400, "desc": "Onion, Capsicum, Corn, Mushroom, Tomato" },
+
+        { "id": "piz-veg-grand-s", "name": "Veggie Grand (S)", "price": 150, "desc": "Onion, Capsicum, Corn, Mushroom, Tomato, Jalapeno, Mozzarella Cheese" },
+        { "id": "piz-veg-grand-m", "name": "Veggie Grand (M)", "price": 300, "desc": "Onion, Capsicum, Corn, Mushroom, Tomato, Jalapeno, Mozzarella Cheese" },
+        { "id": "piz-veg-grand-l", "name": "Veggie Grand (L)", "price": 400, "desc": "Onion, Capsicum, Corn, Mushroom, Tomato, Jalapeno, Mozzarella Cheese" },
+
+        { "id": "piz-tandoori-paneer-s", "name": "Tandoori Paneer (S)", "price": 150, "desc": "Red Peprika, Corn, Tandoori Paneer" },
+        { "id": "piz-tandoori-paneer-m", "name": "Tandoori Paneer (M)", "price": 300, "desc": "Red Peprika, Corn, Tandoori Paneer" },
+        { "id": "piz-tandoori-paneer-l", "name": "Tandoori Paneer (L)", "price": 400, "desc": "Red Peprika, Corn, Tandoori Paneer" },
+
+        { "id": "piz-farm-s", "name": "Farm House Pizza (S)", "price": 150, "desc": "" },
+        { "id": "piz-farm-m", "name": "Farm House Pizza (M)", "price": 300, "desc": "" },
+        { "id": "piz-farm-l", "name": "Farm House Pizza (L)", "price": 400, "desc": "" },
+
+        { "id": "piz-campus-s", "name": "Campus Special Pizza (S)", "price": 170, "desc": "" },
+        { "id": "piz-campus-m", "name": "Campus Special Pizza (M)", "price": 325, "desc": "" },
+        { "id": "piz-campus-l", "name": "Campus Special Pizza (L)", "price": 450, "desc": "" }
+      ]
+    },
+    {
+      "id": "pizza-double-veg",
+      "name": "Double Veg. Pizza",
+      "items": [
+        { "id": "piz-dv-cc-s", "name": "Corn + Capsicum (S)", "price": 99, "desc": "" },
+        { "id": "piz-dv-cc-m", "name": "Corn + Capsicum (M)", "price": 209, "desc": "" },
+        { "id": "piz-dv-cc-l", "name": "Corn + Capsicum (L)", "price": 349, "desc": "" },
+
+        { "id": "piz-dv-ot-s", "name": "Onion + Tomato (S)", "price": 99, "desc": "" },
+        { "id": "piz-dv-ot-m", "name": "Onion + Tomato (M)", "price": 209, "desc": "" },
+        { "id": "piz-dv-ot-l", "name": "Onion + Tomato (L)", "price": 349, "desc": "" },
+
+        { "id": "piz-dv-oc-s", "name": "Onion + Corn (S)", "price": 99, "desc": "" },
+        { "id": "piz-dv-oc-m", "name": "Onion + Corn (M)", "price": 209, "desc": "" },
+        { "id": "piz-dv-oc-l", "name": "Onion + Corn (L)", "price": 349, "desc": "" },
+
+        { "id": "piz-dv-ocap-s", "name": "Onion + Capsicum (S)", "price": 99, "desc": "" },
+        { "id": "piz-dv-ocap-m", "name": "Onion + Capsicum (M)", "price": 209, "desc": "" },
+        { "id": "piz-dv-ocap-l", "name": "Onion + Capsicum (L)", "price": 349, "desc": "" },
+
+        { "id": "piz-dv-tc-s", "name": "Tomato + Corn (S)", "price": 99, "desc": "" },
+        { "id": "piz-dv-tc-m", "name": "Tomato + Corn (M)", "price": 209, "desc": "" },
+        { "id": "piz-dv-tc-l", "name": "Tomato + Corn (L)", "price": 349, "desc": "" }
+      ]
+    },
+    {
+      "id": "pizza-veg-treat",
+      "name": "Veg. Treat",
+      "items": [
+        { "id": "piz-vt-mix-s", "name": "Mix Veg. (S)", "price": 109, "desc": "" },
+        { "id": "piz-vt-mix-m", "name": "Mix Veg. (M)", "price": 209, "desc": "" },
+        { "id": "piz-vt-mix-l", "name": "Mix Veg. (L)", "price": 399, "desc": "" },
+
+        { "id": "piz-vt-po-s", "name": "Paneer + Onion (S)", "price": 109, "desc": "" },
+        { "id": "piz-vt-po-m", "name": "Paneer + Onion (M)", "price": 209, "desc": "" },
+        { "id": "piz-vt-po-l", "name": "Paneer + Onion (L)", "price": 399, "desc": "" },
+
+        { "id": "piz-vt-pc-s", "name": "Paneer + Capsicum (S)", "price": 109, "desc": "" },
+        { "id": "piz-vt-pc-m", "name": "Paneer + Capsicum (M)", "price": 209, "desc": "" },
+        { "id": "piz-vt-pc-l", "name": "Paneer + Capsicum (L)", "price": 399, "desc": "" },
+
+        { "id": "piz-vt-pcorn-s", "name": "Paneer + Corn (S)", "price": 109, "desc": "" },
+        { "id": "piz-vt-pcorn-m", "name": "Paneer + Corn (M)", "price": 209, "desc": "" },
+        { "id": "piz-vt-pcorn-l", "name": "Paneer + Corn (L)", "price": 409, "desc": "" },
+
+        { "id": "piz-vt-pt-s", "name": "Paneer + Tomato (S)", "price": 109, "desc": "" },
+        { "id": "piz-vt-pt-m", "name": "Paneer + Tomato (M)", "price": 209, "desc": "" },
+        { "id": "piz-vt-pt-l", "name": "Paneer + Tomato (L)", "price": 409, "desc": "" }
+      ]
+    },
+    {
+      "id": "pizza-single",
+      "name": "Single Veg. Pizza",
+      "items": [
+        { "id": "piz-corn-s", "name": "Corn (S)", "price": 69, "desc": "" },
+        { "id": "piz-corn-m", "name": "Corn (M)", "price": 169, "desc": "" },
+        { "id": "piz-corn-l", "name": "Corn (L)", "price": 299, "desc": "" },
+
+        { "id": "piz-onion-s", "name": "Onion (S)", "price": 69, "desc": "" },
+        { "id": "piz-onion-m", "name": "Onion (M)", "price": 169, "desc": "" },
+        { "id": "piz-onion-l", "name": "Onion (L)", "price": 299, "desc": "" },
+
+        { "id": "piz-tomato-s", "name": "Tomato (S)", "price": 69, "desc": "" },
+        { "id": "piz-tomato-m", "name": "Tomato (M)", "price": 169, "desc": "" },
+        { "id": "piz-tomato-l", "name": "Tomato (L)", "price": 299, "desc": "" },
+
+        { "id": "piz-capsicum-s", "name": "Capsicum (S)", "price": 69, "desc": "" },
+        { "id": "piz-capsicum-m", "name": "Capsicum (M)", "price": 169, "desc": "" },
+        { "id": "piz-capsicum-l", "name": "Capsicum (L)", "price": 299, "desc": "" },
+
+        { "id": "piz-paneer-s", "name": "Paneer (S)", "price": 69, "desc": "" },
+        { "id": "piz-paneer-m", "name": "Paneer (M)", "price": 199, "desc": "" },
+        { "id": "piz-paneer-l", "name": "Paneer (L)", "price": 359, "desc": "" }
+      ]
+    },
+    {
+      "id": "chai",
+      "name": "Chai",
+      "items": [
+        { "id": "chai-plain", "name": "Plain Chai", "price": 20, "desc": "" },
+        { "id": "chai-dhoodh", "name": "Dhoodh Chai", "price": 30, "desc": "" },
+        { "id": "chai-masala", "name": "Masala Chai", "price": 40, "desc": "" },
+        { "id": "chai-adrak", "name": "Adhrak Chai", "price": 40, "desc": "" },
+        { "id": "chai-long", "name": "Lonng Chai", "price": 45, "desc": "" },
+        { "id": "chai-campus", "name": "Campus Special Chai", "price": 50, "desc": "" }
+      ]
+    },
+    {
+      "id": "coffee",
+      "name": "Coffee",
+      "items": [
+        { "id": "coffee-hot", "name": "Hot Coffee", "price": 50, "desc": "" },
+        { "id": "coffee-classic", "name": "Classic Cold Coffee", "price": 89, "desc": "" },
+        { "id": "coffee-icecream", "name": "Classic with Ice Cream", "price": 119, "desc": "" },
+        { "id": "coffee-caramel", "name": "Caramel Cold Coffee", "price": 120, "desc": "" },
+        { "id": "coffee-campus-hot", "name": "Campus Special Hot Coffee", "price": 60, "desc": "" },
+        { "id": "coffee-campus-cold", "name": "Campus Special Cold Coffee", "price": 125, "desc": "" }
+      ]
+    },
+    {
+      "id": "cold-drink",
+      "name": "Cold Drink",
+      "items": [
+        { "id": "cd-coke-s", "name": "Coke (S)", "price": 30, "desc": "" },
+        { "id": "cd-coke-m", "name": "Coke (M)", "price": 40, "desc": "" },
+        { "id": "cd-dew-s", "name": "Mountain Dew (S)", "price": 30, "desc": "" },
+        { "id": "cd-dew-m", "name": "Mountain Dew (M)", "price": 40, "desc": "" },
+        { "id": "cd-sprite-s", "name": "Sprite (S)", "price": 30, "desc": "" },
+        { "id": "cd-sprite-m", "name": "Sprite (M)", "price": 40, "desc": "" },
+        { "id": "cd-frooti-s", "name": "Frooti (S)", "price": 40, "desc": "" },
+        { "id": "cd-frooti-m", "name": "Frooti (M)", "price": 50, "desc": "" },
+        { "id": "cd-fruit-beer-m", "name": "Fruit Beer (M)", "price": 60, "desc": "" }
+      ]
+    },
+    {
+      "id": "desserts",
+      "name": "Ice Cream",
+      "items": [
+        { "id": "ice-vanilla", "name": "Vanilla", "price": 49, "desc": "" },
+        { "id": "ice-strawberry", "name": "Strawberry", "price": 49, "desc": "" },
+        { "id": "ice-butterscotch", "name": "Butter Scotch", "price": 69, "desc": "" },
+        { "id": "ice-chocolate", "name": "Chocolate", "price": 79, "desc": "" },
+        { "id": "ice-black-current", "name": "Black Current", "price": 79, "desc": "" }
+      ]
+    },
+    {
+      "id": "shakes",
+      "name": "Shakes",
+      "items": [
+        { "id": "shake-banana", "name": "Banana Shake", "price": 79, "desc": "" },
+        { "id": "shake-mango", "name": "Mango Shake", "price": 79, "desc": "" },
+        { "id": "shake-butterscotch", "name": "Butter Scotch Shake", "price": 89, "desc": "" },
+        { "id": "shake-strawberry", "name": "Strawberry Shake", "price": 89, "desc": "" },
+        { "id": "shake-vanilla", "name": "Vanilla Shake", "price": 89, "desc": "" },
+        { "id": "shake-chocolate", "name": "Chocolate Shake", "price": 89, "desc": "" },
+        { "id": "shake-oreo", "name": "Oreo Shake", "price": 109, "desc": "" },
+        { "id": "shake-kitkat", "name": "Kit Kat Shake", "price": 119, "desc": "" },
+        { "id": "shake-blueberry", "name": "Blueberry Shake", "price": 119, "desc": "" },
+        { "id": "shake-black-current", "name": "Black Current Shake", "price": 119, "desc": "" }
+      ]
+    },
+    {
+      "id": "coolers",
+      "name": "Coollers",
+      "items": [
+        { "id": "cool-shikanji", "name": "Shi kanji", "price": 39, "desc": "" },
+        { "id": "cool-masala", "name": "Masala lemonande", "price": 49, "desc": "" },
+        { "id": "cool-mint", "name": "Mint Mojito", "price": 79, "desc": "" },
+        { "id": "cool-kiwi", "name": "Kiwi Mojito", "price": 89, "desc": "" },
+        { "id": "cool-green-apple", "name": "Green Apple Mojito", "price": 89, "desc": "" },
+        { "id": "cool-blue-panther", "name": "Blue Panther Mojito", "price": 89, "desc": "" },
+        { "id": "cool-watermelon", "name": "Watermelon Mojito", "price": 89, "desc": "" }
+      ]
+    },
+    {
+      "id": "combos",
+      "name": "Couple Combo",
+      "items": [
+        { "id": "combo-heart", "name": "Heart Shake Pizza + Fries + 2 Cold Coffee", "price": 449, "desc": "" },
+        { "id": "combo-pizza-burger", "name": "Pizza + Burger + Fries + 2 Small Cold Drinks", "price": 349, "desc": "" }
+      ]
+    }
+  ]
+};
